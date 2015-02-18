4a3c52a134dc6b3eee13d537e30fb79ca7724a38
Amir <amir@amirrajabi.com>
2015-02-18 20:45:29 +1100

    added ng-animation for search items

--- js/controllers.js
+++ js/controllers.js
@@ -2,12 +2,12 @@
  * Created by amirrajabi on 13/02/15.
  */
 
-var movieControllers = angular.module('movieControllers', []);
+var movieControllers = angular.module('movieControllers', ['ngAnimate']);
 
 
-movieControllers.controller('ListController',['$scope', '$http', function($scope, $http){
+movieControllers.controller('ListController', ['$scope', '$http', function ($scope, $http) {
 
-    $http.get('model/data.json').success(function(data){
+    $http.get('model/data.json').success(function (data) {
         $scope.movies = data;
         $scope.sort = 'name';
         $scope.type = 'Sci-Fi';
@@ -16,21 +16,21 @@ movieControllers.controller('ListController',['$scope', '$http', function($scope
 }]);
 
 
-movieControllers.controller('DetailsController',['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+movieControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
 
-    $http.get('model/data.json').success(function(data){
+    $http.get('model/data.json').success(function (data) {
         $scope.movies = data;
         $scope.whichItem = $routeParams.itemId;
 
-        if($routeParams.itemId > 0){
-            $scope.prevItem = Number($routeParams.itemId)-1;
-        }else{
-            $scope.prevItem = $scope.movies.length-1;
+        if ($routeParams.itemId > 0) {
+            $scope.prevItem = Number($routeParams.itemId) - 1;
+        } else {
+            $scope.prevItem = $scope.movies.length - 1;
         }
 
-        if($routeParams.itemId < $scope.movies.length-1){
-            $scope.nextItem = Number($routeParams.itemId)+1;
-        }else{
+        if ($routeParams.itemId < $scope.movies.length - 1) {
+            $scope.nextItem = Number($routeParams.itemId) + 1;
+        } else {
             $scope.nextItem = 0;
         }
 
