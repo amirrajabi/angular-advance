c9a6db4781a29ba3e06dc529514683dc813d5138
Amir <amir@amirrajabi.com>
2015-04-27 22:26:47 +1000

    update new controller

--- phase-2/js/controllers.js
+++ phase-2/js/controllers.js
@@ -1,23 +1,57 @@
 /**
- * Created by amirrajabi on 13/02/15.
+ * Created by amirrajabi on 22/02/15.
  */
 
 var listControllers = angular.module('listControllers', []);
 
-listControllers.controller('MainController', ['$scope', '$http', function ($scope, $http) {
+listControllers.controller('MainController', ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
 
     var onUserComplete = function(response){
         $scope.user = response.data;
+        $http.get($scope.user.repos_url)
+            .then(onRepos, onError);
+    };
+
+    var onRepos = function(response){
+        $scope.repos = response.data;
     };
 
     var onError = function(){
-        $scope.error = 'Could not fetch the user!';
+        $scope.error = 'Could not fetch the data!';
     };
 
-    $http.get('https://api.github.com/users/amirrajabi')
-        .then(onUserComplete, onError);
+    var decrementCountdown = function(){
+        $scope.countdown -= 1;
 
-    $scope.message = 'Good day!';
+        if($scope.countdown < 1){
+            $scope.search($scope.username)
+        }
+    };
 
+    var startCountdown = function(){
+        $interval(decrementCountdown, 1000, $scope.countdown);
+    };
+
+    var getTime = function(){
+        $scope.dateTime = new Date();
+    };
+
+    var time = function(){
+        $interval(getTime, 1000, $scope.dateTime);
+    };
+
+    $scope.search = function(username){
+        $http.get('https://api.github.com/users/' + username)
+            .then(onUserComplete, onError);
+    };
+
+    $scope.username = 'Angular';
+    $scope.message = 'Github Viewer!';
+    $scope.repoSortOrder = '-language';
+    $scope.dateTime = '';
+    $scope.countdown = 5;
+
+    startCountdown();
+    time();
 
 }]);
